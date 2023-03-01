e4b4760219e3ff26aca959aad96a9f3102febd25
Łukasz Lewandowski <lukastonie@gmail.com>
2023-03-01 20:27:10 +0100

    3 cards added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'cards'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cards');
   });
 
   it('should render title', () => {
